dc31dcce7fb4e3030574cb174838579bee107269
Tran Quang Dung <tqd0105@gmail.com>
2026-02-02 17:29:17 +0700

    fix: hobby & skills

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ navbarItems.forEach((item) => {
         let scrollPosition;
         
         // Đối với Projects và Contact, scroll đến vị trí giữa
-        if (targetId === "#projects" || targetId === "#contact" || targetId === "#career") {
+        if (targetId === "#projects" || targetId === "#contact" || targetId === "#career" || targetId === "#skills") {
           scrollPosition = sectionTop - ((windowHeight + 100) / 2) + (sectionHeight / 2);
         } else {
           // Các section khác scroll bình thường với offset navbar
